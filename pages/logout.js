bfc3d1b388b0625cc14cfd18f7062f5750dcd586
ALVlN <jhonata_fsantos@hotmail.com>
2022-05-31 17:26:40 -0300

    LinkPages

--- pages/logout.js
+++ pages/logout.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Link from 'next/link'
+import { useRouter } from 'next/router';
+
 
 const Logout = props => {
+  const router = useRouter();
+  const handleLogout = () => {
+    router.push('/')
+  }
   return (
     <div>
-      <div>
-      <Link href='/'><button>SAIR</button></Link>
-      </div>
+        <h1>Deseja SAIR?</h1>
+        <button onClick={handleLogout}>Deslogar</button>
     </div>
   );
 };
