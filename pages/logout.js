8aa88eae739e94d3aad037eb8ca802605d31e45c
ALVlN <jhonata_fsantos@hotmail.com>
2022-05-31 15:46:01 -0300

    Login e Logout

---
+++ pages/logout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Link from 'next/link'
+
+const Logout = props => {
+  return (
+    <div>
+      <div>
+      <Link href='/'><button>SAIR</button></Link>
+      </div>
+    </div>
+  );
+};
+
+export default Logout;
